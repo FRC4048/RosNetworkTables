85d9cad743c0877162774ecc961a469a84a540ed
goatfanboi23 <python.goat@gmail.com>
2023-01-31 16:53:39 -0500

    moved main code to a separate class and launcher file

---
+++ src/main/java/org/frc/team4048/rosnetworktables/Constants.java
@@ -0,0 +1,7 @@
+package org.frc.team4048.rosnetworktables;
+
+public class Constants {
+     //ENVIRONMENT VARIABLE NAMES
+     public static final String ROS_MASTER_URI = "ROS_MASTER_URI";
+     public static final String NT_IP = "NT_IP";
+}
---
+++ src/main/java/org/frc/team4048/rosnetworktables/Launcher.java
@@ -0,0 +1,7 @@
+package org.frc.team4048.rosnetworktables;
+
+public class Launcher {
+     public static void main(String[] args) {
+          NtRosProxy.get().start();
+     }
+}
