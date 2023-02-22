9d67537fb0f361a52c735bb041a4c8a30f0b9146
ohad <ohadze@yahoo.com>
2023-02-22 00:06:03 -0500

    Changes to docker setup to try and run on pi

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,9 +1,11 @@
 #/bin/sh
 
 mkdir -p contents
-rm contents/*
+rm -r contents/*
+
 cp ../build/libs/RosNetworkTables-1.0-SNAPSHOT.jar contents/RosNetworkTables.jar
 cp ../src/main/resources/config.carrot contents/config.carrot
+unzip ../src/main/lib/wpiutil-cpp-2023.2.1-linuxarm64.zip -d contents/wpilib
 
 docker build -t nt2ros .
 
