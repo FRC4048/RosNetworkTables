4a8bcea31fd71f79a956880af1b1ff18fb681cb0
goatfanboi23 <python.goat@gmail.com>
2023-02-21 21:06:10 -0500

    started to make docker build windows compatible. Also manully added NetworkTables jni becuase NT4 broke arm architure.

--- Docker/build.sh
+++ Docker/build.sh
@@ -4,7 +4,9 @@ mkdir -p contents
 rm contents/*
 cp ../build/libs/RosNetworkTables-1.0-SNAPSHOT.jar contents/RosNetworkTables.jar
 cp ../src/main/resources/config.carrot contents/config.carrot
+cp ../src/main/lib/wpiutil-cpp-2023.2.1-linuxarm64.zip contents/
+unzip contents/wpiutil-cpp-2023.2.1-linuxarm64.zip
 
-docker build -t nt2ros .
+docker build --platform linux/arm64 -t nt2ros .
 
 docker save nt2ros -o nt2ros.tar
