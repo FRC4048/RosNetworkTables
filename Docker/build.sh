7565f10f270e01763fe8ee96ad112c16d42aa32c
ohad <ohadze@yahoo.com>
2023-02-22 23:18:18 -0500

    Install libatomic to support nt

--- Docker/build.sh
+++ Docker/build.sh
@@ -7,6 +7,6 @@ cp ../build/libs/RosNetworkTables-1.0-SNAPSHOT.jar contents/RosNetworkTables.jar
 cp ../src/main/resources/config.carrot contents/config.carrot
 unzip ../src/main/lib/wpiutil-cpp-2023.2.1-linuxarm64.zip -d contents/wpilib
 
-docker build -t nt2ros .
+docker build --platform linux/arm64 -t nt2ros .
 
 docker save nt2ros -o nt2ros.tar
