52eec65f942a0eabcf7a763d390bf3ab71d875d1
goatfanboi23 <python.goat@gmail.com>
2023-05-15 18:28:01 -0400

    fixed typo again

--- Docker/build.bat
+++ Docker/build.bat
@@ -12,5 +12,5 @@ copy %MY_DIR%..\src\main\resources\config.carrot %MY_DIR%contents\config.carrot
 powershell expand-archive %MY_DIR%..\src\main\lib\wpiutil-cpp-2023.2.1-linuxarm64.zip -DestinationPath %MY_DIR%contents\wpilib
 :: build docker image
 docker build --platform linux/arm64 -t nt2ros .
-:: save docker image to a .tar file insted of uploading the image. We do this because some devices that will need the image will not always have an internet connection
+:: save docker image to a .tar file instead of uploading the image. We do this because some devices that will need the image will not always have an internet connection
 docker save nt2ros -o nt2ros.tar
